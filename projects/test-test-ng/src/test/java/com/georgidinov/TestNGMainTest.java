57bb13243392ec8a599869f2a13036f952dddb15
Georgi Dinov <georgidinov1@abv.bg>
2023-04-16 16:17:16 +0300

    basic test ng setup

---
+++ projects/test-test-ng/src/test/java/com/georgidinov/TestNGMainTest.java
@@ -0,0 +1,12 @@
+package com.georgidinov;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+public class TestNGMainTest {
+
+    @Test
+    public void testGetHw() {
+    }
+}
