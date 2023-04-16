b64fe64549e6079ac85298dc5b9deef6ec221e6e
Georgi Dinov <georgidinov1@abv.bg>
2023-04-16 17:13:56 +0300

    added it test example setup using the failsafe plugin

---
+++ projects/test-integration-test/src/main/java/com/georgidinov/Main.java
@@ -0,0 +1,13 @@
+package com.georgidinov;
+
+public class Main {
+
+    private final String hw = "Hello World";
+
+    public static void main(String[] args) {
+    }
+
+    public String getHw() {
+        return hw;
+    }
+}
---
+++ projects/test-integration-test/src/test/java/com/georgidinov/MainTestIT.java
@@ -0,0 +1,11 @@
+package com.georgidinov;
+
+import org.junit.jupiter.api.Test;
+
+class MainTestIT {
+
+    @Test
+    void getHw() {
+        System.out.println("Integration Test Sim");
+    }
+}
